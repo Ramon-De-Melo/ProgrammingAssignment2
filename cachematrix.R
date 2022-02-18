88edec89c2fe6c243da25c8f5af10f091637c1da
Ramon-De-Melo$ git config --global user.email rdemelo65@gmail.com$ git config --listgit config --global user.name Ramon-De-Melo <rdemelo65@gmail.com>
2022-02-18 11:20:21 +0000

    Coursera R_programmingAssignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions cache and obtain the inverse of 
+## matrices, respectively.
 
-## Write a short comment describing this function
+## This function caches the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv = NULL
+  set = function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## This function obtains the inverse of the cached matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv = x$getinv()
+  
+  
+  if (!is.null(inv)){
+    
+    message("getting cached data")
+    return(inv)
+  }
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  x$setinv(inv)
+  
+  return(inv)
 }
+
+## Example
+## The same result is obtained using the solve() function from base R, and 
+## and the makeCacheMatrix and cacheSolve
+
+m <- matrix(rnorm(4), ncol = 2, nrow = 2) # Create matrix
+
+solve(m) # obtain the inverse using solve()
+
+p <- makeCacheMatrix(m); cacheSolve(p) # obtain the inverse using functions
+
